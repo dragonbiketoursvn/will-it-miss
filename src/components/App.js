9b7fce384ae823ddebd663494fbe8cc1bc9de545
dragonbiketoursvn <dragonbiketoursvn@gmail.com>
2021-12-09 21:48:10 +0700

    Changed wording of intro waiting message

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ export default function App() {
     return (
       <p>
         Getting data from NASA right now to check whether something from space
-        is going to hit us. One moment…
+        is going to whack you in the face with its cock. One moment…
       </p>
     );
   }
